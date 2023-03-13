8ff622fc9fd8151b14ba702defe1521d0ec3b530
Icchi16 <duynghiemhn16@gmail.com>
2023-03-13 14:33:04 +0700

    get element width

--- src/components/gameBoard/GameBoard.tsx
+++ src/components/gameBoard/GameBoard.tsx
@@ -1,3 +1,4 @@
+import { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
 import styles from "./GameBoard.module.css";
 import { Col, Row } from "@nextui-org/react";
 
@@ -6,15 +7,33 @@ const GameBoard = () => {
   const SQUARE_ROW: number = 16;
   const SQUARE_LINE: number = 4;
 
+  let boardWidth: number = 0;
+
+  const boardRef: RefObject<any> = useRef();
+
+  const getBoardWidth = () => {
+    if (boardRef.current) {
+      boardWidth = boardRef.current.offsetWidth;
+    }
+  };
+
+  useLayoutEffect(() => {
+    getBoardWidth();
+    window.addEventListener("resize", getBoardWidth);
+    return () => {
+      window.removeEventListener("resize", getBoardWidth);
+    };
+  }, []);
+
   const handleClick = (e: Event) => {
     const box = e.target as HTMLInputElement;
-    console.log(box.id);
+    console.log(box.id, boardWidth, boardRef.current.offsetWidth);
   };
 
   return (
-    <div>
+    <div ref={boardRef}>
       <Row wrap="wrap">
-        {[...Array(SQUARE_ROW * SQUARE_LINE)].map((x, i) => (
+        {[...Array(SQUARE_ROW * SQUARE_LINE)].map((x, i: number) => (
           <div
             className={styles.Square}
             key={i}
